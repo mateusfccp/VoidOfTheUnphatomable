fc58d65f2e8da8ff638fe57e81871d7f90f2e3d9
Mateus Pinto <mateusfccp@gmail.com>
2025-11-02 22:00:52 -0300

    Clean MainMenu

--- source/src/main/java/org/pintoschneider/void_of_the_unfathomable/game/scenes/MainMenu.java
+++ source/src/main/java/org/pintoschneider/void_of_the_unfathomable/game/scenes/MainMenu.java
@@ -2,10 +2,8 @@ package org.pintoschneider.void_of_the_unfathomable.game.scenes;
 
 import org.pintoschneider.void_of_the_unfathomable.engine.Engine;
 import org.pintoschneider.void_of_the_unfathomable.ui.components.Align;
-import org.pintoschneider.void_of_the_unfathomable.ui.components.ConstrainedBox;
 import org.pintoschneider.void_of_the_unfathomable.ui.core.Alignment;
 import org.pintoschneider.void_of_the_unfathomable.ui.core.Component;
-import org.pintoschneider.void_of_the_unfathomable.ui.core.Constraints;
 import org.pintoschneider.void_of_the_unfathomable.ui.core.CrossAxisAlignment;
 
 import java.util.List;
@@ -14,10 +12,10 @@ public final class MainMenu extends SelectionScene {
     @Override
     List<Option> options() {
         return List.of(
-            new Option("Nuevo Juego", () -> Engine.context().sceneManager().push(new InGame())),
+            new Option("Nuevo Juego", this::startNewGame),
             new Option("Cargar Juego"),
             new Option("Configuraciones"),
-            new Option("Salir", () -> Engine.context().sceneManager().pop())
+            new Option("Salir", this::exitGame)
         );
     }
 
@@ -28,13 +26,17 @@ public final class MainMenu extends SelectionScene {
 
     @Override
     public Component build() {
-        return
-            new Align(
-                Alignment.CENTER,
-                new ConstrainedBox(
-                    new Constraints(null, 20, null, null),
-                    super.build()
-                )
-            );
+        return new Align(
+            Alignment.CENTER,
+            super.build()
+        );
+    }
+
+    private void startNewGame() {
+        Engine.context().sceneManager().push(new InGame());
+    }
+
+    private void exitGame() {
+        Engine.context().sceneManager().pop();
     }
 }
