00cf164a3027e27eea6f495c1962ba29a03bfc61
Mateus Pinto <mateusfccp@gmail.com>
2025-11-10 15:07:13 -0300

    Add documentation to DialogScene and make it only closeable through ENTER key

--- source/src/main/java/org/pintoschneider/void_of_the_unfathomable/game/scenes/DialogScene.java
+++ source/src/main/java/org/pintoschneider/void_of_the_unfathomable/game/scenes/DialogScene.java
@@ -12,16 +12,28 @@ import org.pintoschneider.void_of_the_unfathomable.ui.core.EdgeInsets;
 
 import java.util.function.Consumer;
 
+/**
+ * A scene that displays a dialog message to the player.
+ */
 public final class DialogScene implements Scene {
-    final String message;
-    final Consumer<Void> onClose;
+    private final String message;
+    private final Consumer<Void> onClose;
 
-    int pageCount;
-
+    /**
+     * Creates a new dialog scene with the specified message.
+     *
+     * @param message The message to display in the dialog.
+     */
     public DialogScene(String message) {
         this(message, null);
     }
 
+    /**
+     * Creates a new dialog scene with the specified message and a callback for when the dialog is closed.
+     *
+     * @param message The message to display in the dialog.
+     * @param onClose A callback function to be executed when the dialog is closed.
+     */
     public DialogScene(String message, Consumer<Void> onClose) {
         this.message = message;
         this.onClose = onClose;
@@ -40,9 +52,11 @@ public final class DialogScene implements Scene {
 
     @Override
     public void onKeyPress(Key key) {
-        Engine.context().sceneManager().pop();
-        if (onClose != null) {
-            onClose.accept(null);
+        if (key == Key.ENTER) {
+            Engine.context().sceneManager().pop();
+            if (onClose != null) {
+                onClose.accept(null);
+            }
         }
     }
 }
