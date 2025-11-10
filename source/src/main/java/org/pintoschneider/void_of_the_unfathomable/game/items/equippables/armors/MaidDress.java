0178a2c06335f3bca1a9e53960bc7a08d0968f24
Mateus Pinto <mateusfccp@gmail.com>
2025-11-10 19:34:05 -0300

    Implement Maid Dress name and description and paint

--- source/src/main/java/org/pintoschneider/void_of_the_unfathomable/game/items/equippables/armors/MaidDress.java
+++ source/src/main/java/org/pintoschneider/void_of_the_unfathomable/game/items/equippables/armors/MaidDress.java
@@ -1,7 +1,9 @@
 package org.pintoschneider.void_of_the_unfathomable.game.items.equippables.armors;
 
+import org.pintoschneider.void_of_the_unfathomable.game.ColorPalette;
 import org.pintoschneider.void_of_the_unfathomable.game.items.Equippable;
 import org.pintoschneider.void_of_the_unfathomable.game.items.EquippableSlot;
+import org.pintoschneider.void_of_the_unfathomable.ui.core.Paint;
 
 /**
  * The Maid Dress armor item.
@@ -14,11 +16,16 @@ public class MaidDress implements Equippable {
 
     @Override
     public String name() {
-        return "Vestido de sirvienta";
+        return "Vestido de Maid";
     }
 
     @Override
     public String description() {
-        return "¡UwU!\nUn vestido de sirvienta súper lindo que te hace sentir adorable y servicial al mismo tiempo.";
+        return "UwU :3";
+    }
+
+    @Override
+    public Paint playerPaint(Paint paint) {
+        return paint.withForegroundColor(ColorPalette.BLUSH).withBlink(true);
     }
 }
